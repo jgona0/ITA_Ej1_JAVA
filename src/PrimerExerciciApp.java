1a732cf1bb722e306dec5600fe52b863b2f055c9
Xavi <jgona0@gmail.com>
2020-09-23 18:47:41 +0200

    Cambio nomenclatura

--- src/PrimerExerciciApp.java
+++ src/PrimerExerciciApp.java
@@ -1,5 +1,11 @@
 
-public class E1 {
+/*
+ * Primer exercici IT ACADEMY
+ * Xavi Gonzalez JAVA-1
+ */
+
+
+public class PrimerExerciciApp {
 
 	public static void main(String[] args) {
 
@@ -32,13 +38,13 @@ public class E1 {
 		System.out.println(" \n Fase 2:");
 		
 		// Creeu una constant amb el valor de l’any (1948)
-		final int any_traspas = 1948;
+		final int ANY_TRASPAS = 1948;
 		
 		// Creeu una variable que guardi cada quan hi ha un any de traspàs
 		int cada_traspas = 4;
 		
 		// Calculeu quants anys de traspàs hi ha entre 1948 i el vostre any de naixement i emmagatzemeu el valor resultant en una variable
-		int anys_traspas = (any_naixement - any_traspas) / cada_traspas;
+		int anys_traspas = (any_naixement - ANY_TRASPAS) / cada_traspas;
 		
 		// Mostreu per pantalla el resultat del càlcul
 		System.out.println("Hi ha " + anys_traspas + " anys de traspàs");
@@ -60,7 +66,7 @@ public class E1 {
 		String frase_si = "El meu any de naixement és de traspàs";
 		String frase_no = "El meu any de naixement NO és de traspàs";
 		
-		for (int i=any_traspas; i <= any_naixement ; i+=cada_traspas) {
+		for (int i=ANY_TRASPAS; i <= any_naixement ; i+=cada_traspas) {
 			
 			System.out.println(i);
 			
